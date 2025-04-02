3d5eae2ace4a3a7cb76095355df48300b1170d86
JonathasChagas <jonathas252423@gmail.com>
2025-04-02 16:34:49 -0300

    added middleware

--- app/v1/route.ts
+++ app/v1/route.ts
@@ -1,18 +1,8 @@
 import { document } from '@/lib/openapi';
 import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-export function GET(req: NextRequest) {
-	const apiKey = req.headers.get('enem-api-key');
-	
-	if (!apiKey || apiKey !== process.env.ENEM_API_KEY) {
-		return NextResponse.json (
-			{ error: 'Forbidden: Missing or invalid API key' },
-			{ status: 403 }
-		);
-	}
-
+export function GET() {
     return NextResponse.json(document);
 }
